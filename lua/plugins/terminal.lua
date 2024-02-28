4ebc7432be43b5f2972cc8c523eb0e92749be672
Walter Woodall <wwoodal@amazon.com>
2024-02-28 10:39:52 -0800

    Add Floaterm dependency

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,7 @@
+return {
+  "voldikss/vim-floaterm",
+  keys = {
+    { "<leader>fT", "<cmd> FloatermNew<cr>", desc = "Open new terminal" },
+    { "<leader>ft", "<cmd> FloatermToggle<cr>", desc = "Toggle terminal" },
+  },
+}
