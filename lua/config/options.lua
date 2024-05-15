e00ad32dcd8070d0d726bb18d24d2cbba7b63a7e
Walter Woodall <wwoodal@amazon.com>
2024-05-15 16:14:43 +0000

    fix autoformat and update dependencies

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.autoformat = false
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
