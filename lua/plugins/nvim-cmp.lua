89da64be4ffbbfbb5759278575bc29e3652fb535
Walter Woodall <wwoodal@amazon.com>
2024-07-19 19:47:14 +0000

    fix: only run auto-complete after 3 characters for performance reasons

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,13 +1,5 @@
 return {
     "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-emoji",
-        {
-            "Saecki/crates.nvim",
-            event = { "BufRead Cargo.toml" },
-            config = true,
-        },
-    },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
         local has_words_before = function()
@@ -16,17 +8,17 @@ return {
             return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
         end
 
-        local luasnip = require("luasnip")
         local cmp = require("cmp")
 
         opts.mapping = vim.tbl_extend("force", opts.mapping, {
             ["<Tab>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
+                    -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
                     cmp.select_next_item()
-                    -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-                    -- this way you will only jump inside the snippet region
-                elseif luasnip.expand_or_jumpable() then
-                    luasnip.expand_or_jump()
+                elseif vim.snippet.active({ direction = 1 }) then
+                    vim.schedule(function()
+                        vim.snippet.jump(1)
+                    end)
                 elseif has_words_before() then
                     cmp.complete()
                 else
@@ -36,16 +28,19 @@ return {
             ["<S-Tab>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
+                elseif vim.snippet.active({ direction = -1 }) then
+                    vim.schedule(function()
+                        vim.snippet.jump(-1)
+                    end)
                 else
                     fallback()
                 end
             end, { "i", "s" }),
         })
+        opts.sources = cmp.config.sources({
+            { name = "nvim_lsp", keyword_length = 3, max_item_count = 10 },
+            { name = "path" },
+        }, {{ name = "buffer" }, })
 
-        opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-            { name = "crates" },
-        }))
     end,
 }
