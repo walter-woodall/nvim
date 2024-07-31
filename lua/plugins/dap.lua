ca9679942133b11c5dabf068b6f6cdda6e9f0c37
Walter Woodall <wwoodal@amazon.com>
2024-07-31 23:43:12 +0000

    add debug support for Rust

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -6,7 +6,6 @@ dap.configurations.java = {
         name = "Debug (Attach) - Remote";
         hostName = "127.0.0.1";
         port = 8787;
-        projectName = "BigBirdStorageNode";
     },
 }
 return {
@@ -16,7 +15,7 @@ return {
             "williamboman/mason.nvim",
             opts = function(_, opts)
                 opts.ensure_installed = opts.ensure_installed or {}
-                vim.list_extend(opts.ensure_installed, { "java-test", "java-debug-adapter" })
+                vim.list_extend(opts.ensure_installed, { "codelldb", "java-test", "java-debug-adapter" })
             end,
         },
     },
