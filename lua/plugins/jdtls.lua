f4153ee5269f612db614bb8175cb2e2428fcfec0
Walter Woodall <wwoodal@amazon.com>
2024-12-11 20:40:32 +0000

    update jdtls config with bemol

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -1,89 +1,36 @@
-function bemol()
-    local bemol_dir = vim.fs.find({ '.bemol' }, { upward = true, type = 'directory'})[1]
-    local ws_folders_lsp = {}
-    if bemol_dir then
-        local file = io.open(bemol_dir .. '/ws_root_folders', 'r')
-        if file then
-            vim.notify("Loading LSP files from Bemol!!")
-            for line in file:lines() do
-                table.insert(ws_folders_lsp, line)
-            end
-            file:close()
-        end
-    end
-
-    for _, line in ipairs(ws_folders_lsp) do
-        vim.notify(string.format("Adding workspace folder to LSP: %s", line))
-        vim.lsp.buf.add_workspace_folder(line)
-    end
-
-end
-
-
 return {
-    "mfussenegger/nvim-jdtls",
-    opts = function()
-        local mason_registry = require("mason-registry")
-        local lombok_jar = mason_registry.get_package("jdtls"):get_install_path() .. "/lombok.jar"
-        return {
-            -- How to find the root dir for a given filename. The default comes from
-            -- lspconfig which provides a function specifically for java projects.
-            root_dir = LazyVim.lsp.get_raw_config("jdtls").default_config.root_dir,
+  "mfussenegger/nvim-jdtls",
+  opts = function(_, opts)
+    local root_dir = require("jdtls.setup").find_root({ "packageInfo" }, "Config")
 
-            -- How to find the project name for a given root dir.
-            project_name = function(root_dir)
-                return root_dir and vim.fs.basename(root_dir)
-            end,
-
-            -- Where are the config and workspace dirs for a project?
-            jdtls_config_dir = function(project_name)
-                return vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/config"
-            end,
-            jdtls_workspace_dir = function(project_name)
-                return vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/workspace"
-            end,
-
-            -- How to run jdtls. This can be overridden to a full java command-line
-            -- if the Python wrapper script doesn't suffice.
-            cmd = {
-                vim.fn.exepath("jdtls"),
-                "--jvm-arg=-Xms2G",
-                "--jvm-arg=-Xmx8G",
-                "--jvm-arg=-XX:+UseG1GC",
-                string.format("--jvm-arg=-javaagent:%s", lombok_jar),
-            },
-            full_cmd = function(opts)
-                local fname = vim.api.nvim_buf_get_name(0)
-                local root_dir = opts.root_dir(fname)
-                local project_name = opts.project_name(root_dir)
-                local cmd = vim.deepcopy(opts.cmd)
-                if project_name then
-                    vim.list_extend(cmd, {
-                        "-configuration",
-                        opts.jdtls_config_dir(project_name),
-                        "-data",
-                        opts.jdtls_workspace_dir(project_name),
-                    })
-                end
-                return cmd
-            end,
-
-            -- These depend on nvim-dap, but can additionally be disabled by setting false here.
-            dap = { hotcodereplace = "auto", config_overrides = {} },
-            dap_main = {},
-            test = true,
-            settings = {
-                java = {
-                    edit = {
-                        validateAllOpenBuffersOnChanges = false,
-                    },
-                    inlayHints = {
-                        parameterNames = {
-                            enabled = "all",
-                        },
-                    },
-                },
-            },
-        }
+    local workspaces = {}
+    if root_dir then
+      local file = io.open(root_dir .. "/.bemol/ws_root_folders")
+      if file then
+        for line in file:lines() do
+          table.insert(workspaces, "file://" .. line)
+        end
+        file:close()
+      end
     end
+
+    local project_name = opts.project_name(root_dir)
+    local cmd = vim.deepcopy(opts.cmd)
+    if project_name then
+      vim.list_extend(cmd, {
+        "-configuration",
+        opts.jdtls_config_dir(project_name),
+        "-data",
+        opts.jdtls_workspace_dir(project_name),
+      })
+    end
+
+    opts.jdtls = {
+      cmd = cmd,
+      init_options = {
+        workspaceFolders = workspaces,
+      },
+      root_dir = root_dir,
+    }
+  end,
 }
