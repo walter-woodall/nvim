dcc5525ced6733cc527212e26a8b2a0eb8498127
Walter Woodall <wwoodal@amazon.com>
2024-12-16 21:39:19 +0000

    fix debug attach points

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,31 +1,33 @@
-local dap = require('dap')
-dap.configurations.java = {
-    {
-        type = 'java';
-        request = 'attach';
-        name = "Remote Debug (Attach)";
-        hostName = "127.0.0.1";
-        port = 8787;
-    },
-    {
-        type = 'java';
-        request = 'attach';
-        name = "Remote Debug (Attach) - BigBirdStorageNode";
-        hostName = "127.0.0.1";
-        projectName = "BigBirdStorageNode";
-        port = 8787;
-    },
-    {
-        type = 'java';
-        request = 'attach';
-        name = "Remote Debug (Attach) - SaddleJavaClient";
-        hostName = "127.0.0.1";
-        projectName = "SaddleJavaClient";
-        port = 8787;
-    },
-}
 return {
     "mfussenegger/nvim-dap",
+    ops = function ()
+        local dap = require('dap')
+        dap.configurations.java = {
+            {
+                type = 'java';
+                request = 'attach';
+                name = "Debug (Attach) - Remote";
+                hostName = "127.0.0.1";
+                port = 8787;
+            },
+            {
+                type = 'java';
+                request = 'attach';
+                name = "Debug (Attach) - BigBirdStorageNode";
+                hostName = "127.0.0.1";
+                projectName = "BigBirdStorageNode";
+                port = 8787;
+            },
+            {
+                type = 'java';
+                request = 'attach';
+                name = "Debug (Attach) - SaddleJavaClient";
+                hostName = "127.0.0.1";
+                projectName = "SaddleJavaClient";
+                port = 8787;
+            },
+        }
+    end,
     dependencies = {
         {
             "williamboman/mason.nvim",
