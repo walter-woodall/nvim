f0fec7152e602c57ff7f1be64c8d860e40d64456
Walter Woodall <wwoodal@amazon.com>
2024-12-19 03:51:25 +0000

    fix super tab for blink.cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,10 @@
+return {
+    "saghen/blink.cmp",
+    opts = {
+        keymap = {
+            preset = "enter",
+            ["<Tab>"] = { "select_next", "fallback" },
+            ["<S-Tab>"] = { "select_prev", "fallback" },
+        },
+    },
+}
