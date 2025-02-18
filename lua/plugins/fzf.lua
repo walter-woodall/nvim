fb5988ba3c9820e6bf2c7385da0e9f872d7be52e
Walter Woodall <wwoodal@amazon.com>
2025-02-18 23:32:00 +0000

    increase fzf timeout

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,4 +3,9 @@ return {
     keys = {
         { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     },
+    opts = {
+        lsp = {
+            async_or_timeout = 10000
+        },
+    },
 }
