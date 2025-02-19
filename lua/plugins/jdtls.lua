6e9c6ef34ded38c8551905ec6fe6d752f19cf1b6
Walter Woodall <wwoodal@amazon.com>
2025-02-19 18:40:21 +0000

    add symbolInWorkspaceSupport to jdtls config

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -1,36 +1,39 @@
 return {
-  "mfussenegger/nvim-jdtls",
-  opts = function(_, opts)
-    local root_dir = require("jdtls.setup").find_root({ "packageInfo" }, "Config")
+    "mfussenegger/nvim-jdtls",
+    opts = function(_, opts)
+        local root_dir = require("jdtls.setup").find_root({ "packageInfo" }, "Config")
 
-    local workspaces = {}
-    if root_dir then
-      local file = io.open(root_dir .. "/.bemol/ws_root_folders")
-      if file then
-        for line in file:lines() do
-          table.insert(workspaces, "file://" .. line)
+        local workspaces = {}
+        if root_dir then
+            local file = io.open(root_dir .. "/.bemol/ws_root_folders")
+            if file then
+                for line in file:lines() do
+                    table.insert(workspaces, "file://" .. line)
+                end
+                file:close()
+            end
         end
-        file:close()
-      end
-    end
 
-    local project_name = opts.project_name(root_dir)
-    local cmd = vim.deepcopy(opts.cmd)
-    if project_name then
-      vim.list_extend(cmd, {
-        "-configuration",
-        opts.jdtls_config_dir(project_name),
-        "-data",
-        opts.jdtls_workspace_dir(project_name),
-      })
-    end
+        local project_name = opts.project_name(root_dir)
+        local cmd = vim.deepcopy(opts.cmd)
+        if project_name then
+            vim.list_extend(cmd, {
+                "-configuration",
+                opts.jdtls_config_dir(project_name),
+                "-data",
+                opts.jdtls_workspace_dir(project_name),
+            })
+        end
 
-    opts.jdtls = {
-      cmd = cmd,
-      init_options = {
-        workspaceFolders = workspaces,
-      },
-      root_dir = root_dir,
-    }
-  end,
+        opts.jdtls = {
+            cmd = cmd,
+            init_options = {
+                workspaceFolders = workspaces,
+            },
+            extendedClientCapabilities = {
+                symbolInWorkspaceSupport = true,
+            },
+            root_dir = root_dir,
+        }
+    end,
 }
