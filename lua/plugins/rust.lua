ef8fefe0d6b04ef19a02920c09a537f3bb8629b9
Walter Woodall <wwoodal@amazon.com>
2025-04-22 17:53:59 +0000

    fix: force rust to 5.26.0
    AL2 does not support nvim 0.11 yet and rustaceannvim only supports 11
    after version 6.

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,3 +1,10 @@
 return {
-  "Saecki/crates.nvim", enabled = false
+    {
+        "Saecki/crates.nvim", enabled = false
+    },
+    {
+        'mrcjkb/rustaceanvim',
+        version = '5.26.0',
+        lazy = false, -- This plugin is already lazy
+    }
 }
