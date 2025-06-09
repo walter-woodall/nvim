95576013a3d178dc890f1c42208ee53d84a05b02
Walter Woodall <wwoodal@amazon.com>
2025-06-09 23:06:53 +0000

    fix: override on rustaceanvim

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -2,9 +2,4 @@ return {
     {
         "Saecki/crates.nvim", enabled = false
     },
-    {
-        'mrcjkb/rustaceanvim',
-        version = '5.26.0',
-        lazy = false, -- This plugin is already lazy
-    }
 }
