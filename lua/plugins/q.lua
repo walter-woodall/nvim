4bdec085e48147d82186047dfcb63b1fe8433a5f
Walter Woodall <wwoodal@amazon.com>
2025-07-21 21:42:42 +0000

    remove q

--- lua/plugins/q.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        name = 'amazonq',
-        url = 'ssh://git.amazon.com/pkg/AmazonQNVim',
-        opts = {
-            ssoStartUrl = 'https://amzn.awsapps.com/start',
-        },
-    },
-}
